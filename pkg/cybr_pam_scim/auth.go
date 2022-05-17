a5c92d29ef3bc8b484833cf9eef8275ad2c1f4d0
strick-j <joseph.a.strickland@gmail.com>
2022-05-17 12:06:37 -0400

    Added OauthResourceOwner Authentication Option

--- pkg/cybr_pam_scim/auth.go
+++ pkg/cybr_pam_scim/auth.go
@@ -42,3 +42,35 @@ func OauthCredClient(clientID string, clientSecret string, clientAppID string, c
 
 	return authToken, nil
 }
+
+// OauthResourceOwner returns a validated Oauth2 Authentication Token with Refresh Token based on the following provided information:
+//   clientID - Username for the SCIM Application (e.g. "identity-privilege-integration-user$@example.com")
+//   clientSecret - Password for the SCIM Application
+//   clientAppID - ID for the SCIM Application
+//   clientURL - URL for the SCIM Application (e.g. "example.my.idaptive.app")
+//   resourceUsername - Username for the Resource Owner
+//   resourcePassword - Password for the Resource Owner
+func OauthResourceOwner(clientID, clientSecret, clientAppID, clientURL, resourceUsername, resourcePassword string) (*oauth2.Token, error) {
+	endpoint := oauth2.Endpoint{
+		AuthURL:   "https://" + clientURL + "/oauth2/authorize/" + clientAppID,
+		TokenURL:  "https://" + clientURL + "/oauth2/token/" + clientAppID,
+		AuthStyle: 0,
+	}
+
+	// Establish oauth2/clientcredentials config with user provided data
+	var resourceOwnerConfig = oauth2.Config{
+		ClientID:     clientID,
+		ClientSecret: clientSecret,
+		Endpoint:     endpoint,
+		Scopes:       []string{"scim"},
+	}
+
+	ctx := context.Background()
+
+	authToken, err := resourceOwnerConfig.PasswordCredentialsToken(ctx, resourceUsername, resourcePassword)
+	if err != nil {
+		return nil, fmt.Errorf("failed to obtain SCIM Oauth2 Token %w", err)
+	}
+
+	return authToken, nil
+}
